d59462e385e85d3b174c0384c0451e2a29f9cc83
Lucie Contamin <CONTAMIN@pitt.edu>
2022-08-26 12:26:37 -0400

    fix tests age group

--- R/test_agegroup.R
+++ R/test_agegroup.R
@@ -49,47 +49,55 @@ test_agegroup <- function(df, js_def) {
   # - correspond to the age group values as specify in the associated SMH
   # GitHub Repository
   age <- dplyr::distinct(dplyr::select(df, age_group))
-  age <- tidyr::separate(age, age_group, c("min", "max"), sep = "-",
-                         remove = FALSE)
 
-  if (isFALSE(all(age$min %in% js_def$ages$age_min))) {
-    age_min <- paste0(
-      "\U000274c Error 802: The `age_group` column contains unexpected value: ",
-      age$min[!(age$min %in% js_def$ages$age_min)], ". <AGEMIN> can only be: ",
-      paste(js_def$ages$age_min, collapse = ', '), ".")
+  if (is.na(age_writ)) {
+    age <- tidyr::separate(age, age_group, c("min", "max"), sep = "-",
+                           remove = FALSE)
+
+    if (isFALSE(all(age$min %in% js_def$ages$age_min))) {
+      age_min <- paste0(
+        "\U000274c Error 802: The `age_group` column contains unexpected value: ",
+        age$min[!(age$min %in% js_def$ages$age_min)], ". <AGEMIN> can only be: ",
+        paste(js_def$ages$age_min, collapse = ', '), ".")
+    } else {
+      age_min <- NA
+    }
+
+    if (isFALSE(all(age$max %in% js_def$ages$age_max))) {
+      age_max <- paste0(
+        "\U000274c Error 802: The `age_group` column contains unexpected value: ",
+        age$max[!(age$max %in% js_def$ages$age_max)], ". <AGEMAX> can only be: ",
+        paste(js_def$ages$age_max, collapse = ', '), ".")
+    } else {
+      age_max <- NA
+    }
+
+    # -`<AGEMIN>` cannot be equal or greater than` <AGEMAX>`
+    age_test <- dplyr::mutate(
+      age, min = as.numeric(min), max = as.numeric(max)) %>%
+      dplyr::mutate(diff = max - min) %>%
+      dplyr::filter(diff <= 0)
+    if (dim(age_test)[1] > 0) {
+      age_val <- paste0(
+        "\U000274c Error 803: <AGEMIN> cannot be equal or greater than <AGEMAX>",
+        ". Please verify the age_group: ",
+        paste(unique(age_test$age_group),  collapse = ", "), ".")
+    } else {
+      age_val <- NA
+    }
   } else {
     age_min <- NA
-  }
-
-  if (isFALSE(all(age$max %in% js_def$ages$age_max))) {
-    age_max <- paste0(
-      "\U000274c Error 802: The `age_group` column contains unexpected value: ",
-      age$max[!(age$max %in% js_def$ages$age_max)], ". <AGEMAX> can only be: ",
-      paste(js_def$ages$age_max, collapse = ', '), ".")
-  } else {
     age_max <- NA
-  }
-
-  # -`<AGEMIN>` cannot be equal or greater than` <AGEMAX>`
-  age_test <- dplyr::mutate(
-    age, min = as.numeric(min), max = as.numeric(max)) %>%
-    dplyr::mutate(diff = max - min) %>%
-    dplyr::filter(diff <= 0)
-  if (dim(age_test)[1] > 0) {
-    age_val <- paste0(
-      "\U000274c Error 803: <AGEMIN> cannot be equal or greater than <AGEMAX>",
-      ". Please verify the age_group: ",
-      paste(unique(age_test$age_group),  collapse = ", "), ".")
-  } else {
     age_val <- NA
   }
+
 
   # -  either contain one age group  `0-130` OR multiple age groups adding to
   # `0-130` + the overall value for the age group `0-130`
   sel_group <- grep(
     "value|target_end_date|type|model_projection_date|scenario_name|age_group",
     js_def$column_names, invert = TRUE, value = TRUE)
-  lst_df <-  split(df, as.list(df[,sel_group]))
+  lst_df <-  split(df, as.list(df[,sel_group]), drop = TRUE)
   lst_df <- purrr::discard(lst_df, function(x) dim(x)[[1]] < 1)
   overall_test <- lapply(lst_df, function(x) {
     group <- paste(names(unique(x[, sel_group])), ":", unique(x[, sel_group]),
@@ -102,8 +110,8 @@ test_agegroup <- function(df, js_def) {
       overall_test <- NA
     }
     add_age <- grep("0-130", x$age_group, value = TRUE, invert = TRUE)
-    if (length(add_age) > 1) {
-      if (any(!(seq(1, 130) %in% sort(unique(unlist(
+    if (length(add_age) > 0 & all(grepl("\\d{1,2}-\\d{1,3}", add_age))) {
+      if (any(!(seq(0, 130) %in% sort(unique(unlist(
         lapply(strsplit(add_age, "-"), function(x) seq(x[[1]], x[[2]])))))))) {
         sub_group <- paste0(
           "\U0001f7e1 Warning 805: All age group are not adding to 0-130",
@@ -114,25 +122,28 @@ test_agegroup <- function(df, js_def) {
     } else {
       sub_group <- NA
     }
-    all_test <- unique(sub_group, overall_test)
+    all_test <- unique(c(sub_group, overall_test))
     return(all_test)
   })
   if (length(na.omit(unlist(overall_test))) > 100) {
     overall_test <- unique(na.omit(unlist(overall_test))) %>%
-      gsub(" target : \\d{1,2} wk ahead ", " target: ", .) %>% unique()
+      gsub(" target : \\d{1,2} wk ahead ", " target: ", .)  %>%
+      gsub(" quantile : (\\d?(\\.\\d+)?|point)(,)?", "", .) %>%
+      gsub(",$", "", .) %>% unique()
   }
 
-  #- targets woth specific age_group does not contains additional age_group
-  age_target <- names(purrr::keep(purrr::map(
-    c(js_def$required_targets, js_def$optional_targets), "age_group"),
-    function(x) x != "all"))
+  #- targets with specific age_group does not contains additional age_group
+  age_target <- names(purrr::discard(purrr::map(
+    c(js_def$targets$required, js_def$targets$optional), "agegroup"),
+    function(x) any(x == "all")))
   targ_age_test <- lapply(age_target, function(x) {
-    test <- c(js_def$required_targets, js_def$optional_targets)[[x]]$age_group
+    test <- unique(na.omit(unlist(c(js_def$targets$required,
+                                    js_def$targets$optional)[[x]]$agegroup)))
     df_test <- dplyr::filter(df, !grepl(paste(test, collapse = "|"), age_group),
                              grepl(x, target))
     if (dim(df_test)[1] > 0) {
       age_test <- paste0(
-        "\U000274c Error 806: The target ", x, " should only contain the ",
+        "\U0001f7e1 Warning 806: The target ", x, " should only contain the ",
         "agegroup(s): ", paste(test, collapse = ", "),
         ", the data frame contains other age_group (",
         paste(unique(df_test$age_group), collapse = ", "), "), please verify.")
