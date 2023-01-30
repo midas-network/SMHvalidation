6dfc6e3db1dae04d1ccd051b4ed72f46d3307ce5
Lucie Contamin <CONTAMIN@pitt.edu>
2023-01-30 16:31:49 -0500

    typo

--- R/test_sample.R
+++ R/test_sample.R
@@ -73,7 +73,7 @@ test_sample <- function(df, task_ids) {
   if (length(na.omit(unlist(sample_unique))) > 100) {
     sample_unique <- paste0(unique(unlist(purrr::map(strsplit(
       sample_unique, "please verify: "), 1))), length(sample_unique),
-      " unique groups have been idenfied with this issue. For example: ",
+      " unique groups have been identified with this issue. For example: ",
       paste("group: ", head(purrr::map(strsplit(sample_unique, "verify: "), 2),
                             3), collapse = "; "), "; etc.")
   }
