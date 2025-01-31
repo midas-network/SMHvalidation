1a6224a13479d66ff08e96c5423c99d3cb4112dc
LucieContamin <CONTAMIN@pitt.edu>
2025-01-31 17:37:48 -0500

    update plots functions
    - rest of the function/logic to fix to match the update
    - package not working temporary

---
+++ R/generate_validation_plots.R
@@ -0,0 +1,86 @@
+#' Generate Validation Plots
+#'
+#' @param path_proj path to submission file(s) in the expected Scenario
+#'. Modeling Hub standard format
+#' @param target_data data frame containing the
+#' observed data. We highly recommend to use the output of the
+#' `pull_gs_data()` function. The data frame should have the time
+#' series hubverse format.
+#' Set to `NULL` (default), to NOT include comparison with observed data.
+#' @param save_path character vector, path to the saving folder for the PDF
+#'    output
+#' @param y_sqrt boolean, by default FALSE
+#' @param plot_quantiles numeric vector, quantiles to use for plotting (should
+#'    correspond to the quantiles from the `proj_data` parameter)
+#' @param partition vector, for csv and parquet files, allow to validate files
+#' in a partition format, see `arrow` package for more information, and
+#' `arrow::write_dataset()`, `arrow::open_dataset()` functions.
+#'
+#' @importFrom tidyr separate matches
+#' @importFrom dplyr mutate collect
+#' @importFrom arrow open_dataset
+#'
+#' @export
+generate_validation_plots <- function(path_proj, target_data = NULL,
+                                      save_path = dirname(path_proj),
+                                      y_sqrt = FALSE,
+                                      plot_quantiles = c(0.025, 0.975),
+                                      partition = NULL) {
+
+  # SETUP
+  date_pttrn <- "[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}"
+  file_extension <- ".csv|.zip|.gz|.pq|.parquet"
+  if (is.null(partition)) {
+    file_ <- basename(path_proj)
+    team_model_name <- gsub(paste0(date_pttrn, "(_|-)|", file_extension),
+                            "",  file_)
+  } else {
+    file_ <- dir(path_proj, recursive = TRUE)
+    team_model_name <- gsub(paste0("(.+?)?", date_pttrn, "(\\/.*|-)|",
+                                   file_extension),  "", basename(file_))
+  }
+
+  projection_date <- gsub(paste0("(?<=", date_pttrn, ").+"), "", file_,
+                          perl = TRUE)
+  projection_date <- unique(as.Date(projection_date))
+  save_path <- file.path(unique(save_path),
+                         paste0(projection_date, "_", unique(team_model_name),
+                                "_plots.pdf"))
+
+  # Ground Truth Data
+  if (!is.null(target_data)) {
+    target_data <- tidyr::separate(target_data, tidyr::matches("signal"),
+                                   into = c("type", "outcome"), sep = " ") |>
+      dplyr::mutate(pre_gs_end = .data[["date"]] < projection_date)
+  }
+
+  # Projections - Read files
+  if (is.null(partition)) {
+    proj_data <- suppressMessages(read_files(path_proj))
+  } else {
+    proj_data <- load_partition_arrow(path_proj, partition = partition)
+  }
+
+  # Projection - Standardized format
+  proj_data <-
+    dplyr::filter(proj_data,
+                  grepl(paste0("inc case|inc death|inc hosp|inc inf",
+                               "cum case|cum death|cum hosp|inc inf"),
+                        .data[["target"]]) &
+                    grepl("quantile", .data[["output_type"]])) |>
+    dplyr::mutate(date = as.Date(.data[["origin_date"]]) - 1 +
+                    (.data[["horizon"]] * 7),
+                  origin_date = as.Date(.data[["origin_date"]]))
+
+  #remove artifact column
+  if ("X" %in% colnames(proj_data)) {
+    proj_data <- dplyr::select(proj_data, -tidyr::matches("X"))
+  }
+
+  # Create PDF of State Plots
+  make_state_plot_pdf(proj_data = proj_data, target_data = target_data,
+                      team_model_name = team_model_name,
+                      projection_date = unique(proj_data$origin_date),
+                      save_path = save_path, plot_quantiles = plot_quantiles,
+                      y_sqrt = y_sqrt)
+}
---
+++ R/read_files.R
@@ -0,0 +1,38 @@
+#' Read CSV, ZIP, GZ pr PQT files
+#'
+#' Reads a file and output a data frame. The function can read files in CSV,
+#'  ZIP, GZ and PQT format
+#'
+#' @param path path of the file which the date are to be read from
+#'
+#' @importFrom utils read.csv unzip
+#' @importFrom arrow read_parquet
+#'
+#' @examples
+#' \dontrun{
+#'  df <- read_files("PATH/TO/FILE")
+#' }
+#'
+#' @importFrom arrow read_parquet
+#'
+#' @export
+read_files <- function(path, na_string = c("", "NA", "NaN"), sep = ",") {
+  if (grepl(".csv$", basename(path)))
+    df <- read.csv(path, sep = sep, na.strings = na_string)
+  if (grepl(".zip$", basename(path))) {
+    file_name <- unzip(path, list = TRUE)[, "Name", TRUE]
+    unzip(path)
+    df <- read.csv(file_name[1], sep = sep, na.strings = na_string)
+    file.remove(file_name)
+  }
+  if (grepl(".gz$", basename(path))) {
+    file_name <- gzfile(path)
+    df <- read.csv(file_name, sep = sep, na.strings = na_string)
+  }
+  if (grepl(".pqt$|.parquet$", basename(path))) {
+    df <- arrow::read_parquet(path, as_data_frame = TRUE)
+  }
+  if (any("location" %in% names(df))) df$location <- as.character(df$location)
+  df <- factor_columns(df)
+  return(df)
+}
