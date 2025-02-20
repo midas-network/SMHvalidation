d650787f0efbd7fe8d5faba2f6764369b7bbfa99
LucieContamin <CONTAMIN@pitt.edu>
2025-02-20 16:54:56 -0500

    fix age group test

--- R/test_agegroup.R
+++ R/test_agegroup.R
@@ -1,3 +1,66 @@
+# Run all the age group specific column tests
+#'@importFrom hubValidations capture_check_info capture_check_cnd
+#'@importFrom dplyr filter
+#'@importFrom purrr keep map
+#'@noRd
+check_age_group <- function(mtask, df, checks, file_path) {
+  # Prerequisite
+  req_agegroup <- mtask$task_ids$age_group$required
+  opt_agegroup <- unique(mtask$task_ids$age_group$optional)
+  all_agegroup <- unique(c(req_agegroup, opt_agegroup))
+  checks$info <-
+    capture_check_info(file_path,
+                       paste("Age group validation for {.var ",
+                             paste(unique(unlist(mtask$task_ids$target)),
+                                   collapse = ", "), "} target, {.var ",
+                             paste(names(mtask$output_type)), "} output type"))
+  df_test <-
+    dplyr::filter(df, .data[["target"]] %in%
+                    unique(unlist(mtask$task_ids$target)) &
+                    .data[["output_type"]] %in% names(mtask$output_type))
+  if (dim(df_test)[1] > 0) {
+    # - age group written `<AGEMIN>-<AGEMAX>`
+    format_checks <- !(length(grep("\\d{1,2}-\\d{1,3}", df_test$age_group,
+                                   value = TRUE, invert = TRUE)) > 0)
+    msg_dt <-
+      paste0("The column should be written in a {.val <AGEMIN>-<AGEMAX>} ",
+             "format.")
+    checks$agegroup_format <-
+      capture_check_cnd(format_checks, file_path,
+                        msg_subject = "{.var age_group}", details = msg_dt,
+                        msg_attribute = "in a valid format.", error = TRUE)
+    # -- contains expected value
+    age_vect <- unique(df_test$age_group)
+    req_check <- all(age_vect %in% all_agegroup)
+    msg_dt <- NULL
+    if (isFALSE(req_check))
+      msg_dt <- paste0("{.val ", paste(age_vect[!(age_vect %in% all_agegroup)],
+                                       collapse = ", "),
+                       "} not accepted value.")
+    checks$expect_agegroup_value <-
+      capture_check_cnd(format_checks, file_path, error = TRUE,
+                        msg_subject = "{.var age_group}",
+                        msg_verbs = c("contains expected",
+                                      "contains unexpected"),
+                        msg_attribute = "value.", details = msg_dt)
+    req_check <- all(req_agegroup %in% age_vect)
+    msg_dt <- NULL
+    if (isFALSE(req_check))
+      msg_dt <- paste0("{.val ",
+                       paste(age_vect[!(age_vect %in% all_agegroup)],
+                             collapse = ", "), "} is missing.")
+    checks$require_agegroup_value <-
+      capture_check_cnd(format_checks, file_path,  error = TRUE,
+                        msg_subject = "{.var age_group}",
+                        msg_verbs = c("has all", "is missing"),
+                        msg_attribute = "required values.", details = msg_dt)
+  } else {
+    checks <- NULL
+  }
+  return(checks)
+}
+
+
 #' Runs Validation Checks on the `age_group` column
 #'
 #' Validate Scenario Modeling Hub submissions: test if the `age_group` column
@@ -5,7 +68,8 @@
 #'
 #'@param df data frame to test
 #'@param model_task list containing round information for each id columns
-#' and model output (type, format, etc.)
+#' and model output (type, format, etc.) for a specific round
+#'@param file_name optional, name of the file tested
 #'
 #'@details  This function contains 3 tests:
 #' * Age group: If the submission contains projection by age group,
@@ -20,76 +84,13 @@
 #' Function called in the `validate_submission()` function, only if the
 #' submission contains `"age_group"` column
 #'
-#'@importFrom stats na.omit
-#'@importFrom dplyr distinct select mutate %>%
-#'@importFrom tidyr separate
-#'@importFrom purrr discard map keep
+#'@importFrom hubValidations new_hub_validations
+#'@importFrom purrr keep map
 #'@export
-test_agegroup <- function(df, model_task) {
+test_agegroup <- function(df, model_task, file_name = "") {
 
-  test_age <- lapply(model_task, function(x) {
-    # Prerequisite
-    req_agegroup <- x$task_ids$age_group$required
-    opt_agegroup <- unique(x$task_ids$age_group$optional)
-    all_agegroup <- unique(c(req_agegroup, opt_agegroup))
-    df_test <-
-      data.table::data.table(df)[target %in%
-                                 unique(unlist(x$task_ids$target)) &
-                                 output_type %in% names(x$output_type)]
-
-    if (dim(df_test)[1] > 0) {
-      # - age group written `<AGEMIN>-<AGEMAX>`
-      if (length(unique(grep("\\d{1,2}-\\d{1,3}", df_test$age_group,
-                             value = TRUE, invert = TRUE))) > 0) {
-        age_writ <-
-          paste0("\U000274c Error 801: The `age_group` column should contain ",
-                 "value written: 'AGEMIN-AGEMAX', one or more 'age_group' ",
-                 "value(s) is not corresponding in the submission file, please",
-                 " verify: '",
-                 paste(unique(grep("\\d{1,2}-\\d{1,3}", df_test$age_group,
-                                   value = TRUE, invert = TRUE)),
-                       collapse = "', '"), "'.")
-      } else {
-        age_writ <- NA
-      }
-
-      age_vect <- unique(df_test$age_group)
-      if (isFALSE(all(age_vect %in% all_agegroup))) {
-        age_all <- paste0("\U000274c Error 802: The `age_group` column ",
-                          "contains unexpected value: '",
-                          paste(age_vect[!(age_vect %in% all_agegroup)],
-                                collapse = "', '"),
-                          "'. 'age_group' can only be: ",
-                          paste(all_agegroup, collapse = ", "),
-                          ", for the target: ",
-                          paste(unique(unlist(x$task_ids$target)),
-                                collapse = ", "), ".")
-      } else {
-        age_all <- NA
-      }
-      if (isFALSE(all(req_agegroup %in% age_vect))) {
-        age_req <- paste0("\U000274c Error 802: The `age_group` column is ",
-                          "missing at least 1 required value: '",
-                          paste(req_agegroup[!(req_agegroup %in% age_vect)],
-                                collapse = "', '"),
-                          "'. 'age_group' should contain: ",
-                          paste(req_agegroup, collapse = ", "),
-                          ", for the target: ",
-                          paste(unique(unlist(x$task_ids$target)),
-                                collapse = ", "), ".")
-      } else {
-        age_req <- NA
-      }
-      test_age <- unique(na.omit(c(age_writ, age_all, age_req)))
-    } else {
-      test_age <- NA
-    }
-    return(test_age)
-  })
-
-  test_age <- unique(na.omit(unlist(test_age)))
-  if (length(test_age) == 0)
-    test_age <- "No errors or warnings found on Age_group"
-
-  return(test_age)
+  checks <- hubValidations::new_hub_validations()
+  test_age <- purrr::map(model_task, check_age_group, df, checks, file_name)
+  print(purrr::keep(test_age, ~ !is.null(.x)))
+  invisible(NULL)
 }
