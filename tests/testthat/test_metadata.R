633a9c8730226adc78eebe1a0fecb3dbc13eda39
LucieContamin <CONTAMIN@pitt.edu>
2025-03-24 16:53:12 -0400

    add tests

---
+++ tests/testthat/test_metadata.R
@@ -0,0 +1,21 @@
+test_that("Test validation metadata process", {
+
+  hub_path <- "../exp/"
+  check <- SMHvalidation::validate_part_file(hub_path, "t3-mc",
+                                             c("origin_date", "target"))
+  test <- unique(purrr::map_vec(purrr::map(check, ~attr(.x, "class")), 1))
+  expect_equal(test, c("check_success"))
+
+  check <- SMHvalidation::validate_part_file(hub_path, "team2-modelb", NULL)
+  test <- unique(purrr::map_vec(purrr::map(check, ~attr(.x, "class")), 1))
+  expect_equal(test, c("check_success"))
+
+  check <- SMHvalidation::validate_part_file(hub_path, "team2-modelb",
+                                             "origin_date")
+  expect_contains( attr(check$partition_structure, "class"), "check_error")
+
+  check <- SMHvalidation::validate_part_file(hub_path, "t2-modelb",
+                                            "origin_date")
+  expect_contains( attr(check$file_exists, "class"), "check_error")
+
+})
